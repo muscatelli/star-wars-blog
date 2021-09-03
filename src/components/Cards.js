6f0bcc3b34216587aae9038a0dae2466a9386ae3
muscatelli <muscatelli92@gmail.com>
2021-09-02 22:58:28 -0500

    starwars.3

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,15 +3,21 @@ import { Link } from 'react-router-dom';
 
 const Cards = (props) => {
     return (
-        <div className="card">   
-                
-                    <img className="card-img-top" src={props.image} alt="Card image cap" />
-                    <div className="card-body">
-                        <h5 className="card-title">{props.data.name}</h5>
-                        <p className="card-text">{props.data.url}</p>
-                        <Link to="#" className="btn btn-primary">Learn more</Link>
-                    </div>
-                   
+        <div className="card">
+
+
+            <div className="card-body">
+                <img className="card-img-top" src={props.image} alt="Card image cap" />
+                <h5 className="card-title">{props.data.name}</h5>
+                <p className="card-text">{props.data.url}</p>
+                <p className="card-text">Terrain: {props.data.terrain}</p>
+                <p className="card-text">Model: {props.data.model}</p>
+                <p className="card-text">Gender: {props.data.gender}</p>
+                <p className="card-text">Eye Color: {props.data.eye_color}</p>
+                <p className="card-text">Hair Color: {props.data.hair_color}</p>
+                <Link to="#" className="btn btn-primary">Learn more</Link>
+            </div>
+
         </div>
     )
 }
@@ -21,4 +27,3 @@ export default Cards;
 
             //  {JSON.stringify(props.data)}
             //{props.data.name}
-         
